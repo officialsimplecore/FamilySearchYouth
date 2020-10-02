de53b5da127352da6e324796f5dc37709b642f3e
Justin V. - SC <justin@simplecore.org>
2020-10-01 18:06:37 -0700

    Style: Refactor variable names for clarity

--- CoordinateParserBoomer/CoordinateParserBoomer/Program.cs
+++ CoordinateParserBoomer/CoordinateParserBoomer/Program.cs
@@ -19,33 +19,40 @@ namespace CoordinateParserBoomer
     {
         static void Main(string[] args)
         {
+            // Takes input from text file (because of length)
             var input = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\input.txt");
-            String[] coordinatesInput = input.Split("0.0 ");
-            int length = coordinatesInput.Length;
-            Coordinate[] coordinates = new Coordinate[length];
+            String[] fullstringCoordinates = input.Split("0.0 ");
+            int amountOfCoordinates = fullstringCoordinates.Length;
+            
+            // Initialize size of output
+            Coordinate[] coordinates = new Coordinate[amountOfCoordinates];
 
-
-            int position = 0;
-            foreach (string coordinate in coordinatesInput)
+            int pointer = 0;
+            foreach (string coordinate in fullstringCoordinates)
             {
-                String[] tempCoordinate = coordinate.Split(",");
-                coordinates[position] = new Coordinate
+                string[] _coordinate = coordinate.Split(",");
+
+                // Set temporary coordinates to output array
+                coordinates[pointer] = new Coordinate
                 {
-                    Lat = double.Parse(tempCoordinate[1]),
-                    Lng = double.Parse(tempCoordinate[0])
+                    Lat = double.Parse(_coordinate[1]),
+                    Lng = double.Parse(_coordinate[0])
                 };
 
-                Console.WriteLine($"{coordinates[position].Lat} {coordinates[position].Lng}");
+                Console.WriteLine($"{coordinates[pointer].Lat} {coordinates[pointer].Lng}");
 
-                position++;
+                pointer++;
             }
 
             // JSON Serialize
+
+            // Use camel case
             var serializerSettings = new JsonSerializerSettings();
             serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             string json = JsonConvert.SerializeObject(coordinates, serializerSettings);
 
+            // Output JSON object as a file
             System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\coordinates.json", json);
 
             Console.ReadKey();
