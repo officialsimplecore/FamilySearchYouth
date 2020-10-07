b8e58d8a7be8c4386aa06b355f578fb42f9cafd6
Justin V. - SC <justin@simplecore.org>
2020-10-07 08:33:04 -0700

    Add footer

--- FamilySearchYouthProject/src/app/app.module.ts
+++ FamilySearchYouthProject/src/app/app.module.ts
@@ -7,12 +7,12 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { RegionalComponent } from './views/regional/regional.component';
 import { EntranceMapComponent } from './views/entrance-map/entrance-map.component';
 import { CardComponent } from './components/card/card.component';
-import {environment} from '../environments/environment';
 
 // Components
 import {GoogleMapsModule} from '@angular/google-maps';
 import {HttpClientModule} from '@angular/common/http';
 import { AdminComponent } from './views/admin/admin.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,8 @@ import { AdminComponent } from './views/admin/admin.component';
     RegionalComponent,
     EntranceMapComponent,
     CardComponent,
-    AdminComponent
+    AdminComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
